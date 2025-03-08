df9f2d2aa5aae867f74f8fb6a4a1fff44c58af83
luke-brosnan-cbc <luke.brosnan.cbc@gmail.com>
2025-03-08 00:12:07 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sketchlib",
-    version="0.1.0",
+    version="0.1.1",
     description="A collection of efficient matrix sketching methods",
     author="Luke Brosnan",
     author_email="luke.brosnan.cbc@gmail.com",
