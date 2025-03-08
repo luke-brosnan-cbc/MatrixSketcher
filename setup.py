689fafa0763706f316fbc689999924380329c899
luke-brosnan-cbc <luke.brosnan.cbc@gmail.com>
2025-03-08 00:49:01 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,12 +15,10 @@ setup(
         "scipy",
     ],
         classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13"   
     ],
     python_requires=">=3.10",
 )
