c0b55f9a7dd9f1fa861e4c2cd777deaf78aeff6c
luke-brosnan-cbc <luke.brosnan.cbc@gmail.com>
2025-03-17 22:28:41 +0000

    Update __init__.py

--- matrixsketcher/__init__.py
+++ matrixsketcher/__init__.py
@@ -5,6 +5,7 @@ from .countsketch import countsketch
 from .Fast_Walsh-Hadamard_Transform import fwht
 from .leverage_score import leverage_score_sampling
 from .cur_decomposition import cur_decomposition
+from .bicriteria_cur import bicriteria_cur
 
 
 __all__ = [
@@ -12,4 +13,5 @@ __all__ = [
     "fwht",
     "leverage_score_sampling",
     "cur_decomposition",
+    "bicriteria_cur"
 ]
